42c9ca7fb9046423e0a71e02ebaa8e9279d04ae1
Tommy Yu <y@metatoaster.com>
2013-04-06 00:56:41 +1300

    Documentation in the class and function
    - Also renamed the inner function that is returned.

--- mtj/f3u1/factory.py
+++ mtj/f3u1/factory.py
@@ -2,13 +2,35 @@ from sys import maxint
 
 def units_factory(subject, size, higher_unit=None, plural=None,
         omissible=False, higher_omissible=True):
+    """
+    A function that returns a function that will format a number with its
+    units.
+
+    Parameters:
+
+    subject
+        The name of the unit to be displayed
+    size
+        The size of this unit in relation to the base unit.
+    higher_unit
+        A function generated using this method that represents the next
+        higher unit of measurement for this unit type.
+    plural
+        The plural form of the name of this unit.
+    omissible
+        Whether this output is omissible if the size is 0.
+        Default is False.
+    higher_omissible
+        Whether the higher order value is omissible.
+        Default is True.
+    """
 
     assert higher_unit is None or callable(higher_unit)
 
     if plural is None:
         plural = subject
 
-    def _method(value, omissible=omissible):
+    def unit_method(value, omissible=omissible):
         if higher_unit:
             result = higher_unit(value, omissible=higher_omissible)
             higher_size = higher_unit.size
@@ -24,12 +46,21 @@ def units_factory(subject, size, higher_unit=None, plural=None,
         result.append('%d %s' % (derived, derived == 1 and subject or plural))
         return result
 
-    _method.__name__ = subject
-    _method.size = size
-    return _method
+    unit_method.__name__ = subject
+    unit_method.size = size
+    return unit_method
 
 
 class OrderedUnitGroup(object):
+    """
+    Instances of this class is constructed using a list of definitions
+    for a set of related units (in order to make sense) and constructs
+    an object with parameters with the name of the units, that when
+    invoked, will return a human readable string down to that particular
+    unit's size.
+
+    See the accompanied units module for more examples.
+    """
 
     def __init__(self, *a):
         last = None
